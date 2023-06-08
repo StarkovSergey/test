a8c11fc60753a43400e7cdcfbe3a12fec39de8a1
Starkov Sergey <starkovsr@gmail.com>
2023-06-08 20:28:52 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,5 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cards-front/",
   plugins: [react()],
-  server: {
-    open: true,
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests",
-    mockReset: true,
-  },
 })
